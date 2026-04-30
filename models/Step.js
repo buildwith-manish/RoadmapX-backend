7c345d13556fbefebdfccd316e701854223c5614
manishnkotian11 <manishnkotian11@gmail.com>
2026-04-30 16:12:01 +0530

    restore: Step.js with correct ref Roadmap (was deleted by remote)

---
+++ models/Step.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const stepSchema = new mongoose.Schema({
+  roadmapId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Roadmap',
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+  completedAt: {
+    type: Date,
+    default: null,
+  },
+});
+
+module.exports = mongoose.model('Step', stepSchema);
